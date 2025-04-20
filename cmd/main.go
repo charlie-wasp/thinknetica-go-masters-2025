a0a379c596342616ca4be27438b3b87801187614
Timur Ramazanov <charlie.vmk@gmail.com>
2025-04-20 17:02:36 +0300

    http api with errors

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/charlie-wasp/go-masters-2025/internal/api"
+	"github.com/charlie-wasp/go-masters-2025/internal/logger"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	logger.Init(zerolog.DebugLevel)
+	port := 8080
+
+	s := api.New(port)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill, syscall.SIGTERM)
+	defer cancel()
+
+	go func() {
+		log.Info().Msgf("server is listening to port %d", port)
+		err := s.Serve(ctx)
+		if err != nil {
+			log.Fatal().Err(err).Msg("server error")
+		}
+	}()
+
+	<-ctx.Done()
+	s.Shutdown()
+}
